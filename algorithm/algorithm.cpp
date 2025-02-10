83616354ada2b217805cb89d68db64251b9e6635
zain-23 <zaingit780@gmail.com>
2025-02-10 23:45:06 +0500

    Add vector support in VSCode settings and implement sorting algorithm for car names

---
+++ algorithm/algorithm.cpp
@@ -0,0 +1,8 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+
+// Create a vector called cars that will store strings
+std::vector<std::string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+std::sort(cars.begin(), cars.end());
